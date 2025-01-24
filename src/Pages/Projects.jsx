4ccfc5b640c3284013ff6e83f137573d77ff74c9
Shafiq-ur-Rehman <shafeeejakhro1090@gmail.com>
2025-01-24 11:03:20 +0500

    Update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -43,7 +43,6 @@ const Projects = () => {
         </ul>
       ),
       imageUrl:
-        // shafee change project picture
         "https://drive.google.com/file/d/1oH3yl81tJEdRHL9LmRVIU0WKrZ_lu7hB/view?usp=drive_link", //
       link: "https://medium.com/@shafeeejakhro1090/simplifying-local-governance-with-a-java-based-union-council-management-system-b79eb3e0804e", // Replace with your project's GitHub or live link
     },
